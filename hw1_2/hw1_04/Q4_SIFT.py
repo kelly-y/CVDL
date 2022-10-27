19095b0f68f22f8f4cd8de6ef8f2965bfe1531be
kelly-y <kellyyang@csie.io>
2022-10-28 00:41:45 +0800

    Modify img_show color

--- hw1_2/hw1_04/Q4_SIFT.py
+++ hw1_2/hw1_04/Q4_SIFT.py
@@ -15,17 +15,18 @@ def keypoints(imgPath):
 def matchKeypoints(img1Path, img2Path):
     img1 = cv2.imread(img1Path)
     img2 = cv2.imread(img2Path)
-    img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
-    img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+    img1g = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    img2g = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
 
     sift = cv2.xfeatures2d.SIFT_create()
-    kpt1, des1 = sift.detectAndCompute(img1,None)
-    kpt2, des2 = sift.detectAndCompute(img2,None)
+    kpt = sift.detect(img1g, None)
+    imgKpt = cv2.drawKeypoints(img1, kpt, 0, (0, 255, 255))
+    kpt1, des1 = sift.detectAndCompute(img1g,None)
+    kpt2, des2 = sift.detectAndCompute(img2g,None)
 
     # BFMatcher with default params
     bf = cv2.BFMatcher()
     matches = bf.knnMatch(des1, des2, k=2)
-    print(len(matches[0]))
 
     # Apply ratio test
     good = []
@@ -33,7 +34,7 @@ def matchKeypoints(img1Path, img2Path):
         if m.distance < 0.75*n.distance:
             good.append([m])
 
-    img = cv2.drawMatchesKnn(img1, kpt1, img2, kpt2, good, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+    img = cv2.drawMatchesKnn(imgKpt, kpt1, img2, kpt2, good, None, matchColor=(255, 255, 0), flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
     cv2.imshow("Match", img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
