3dab522a7a4b8457bccaf7bb4cfbaa1759c84e42
kelly-y <kellyyang@csie.io>
2022-10-28 22:01:12 +0800

    showImg & showAugmentation

---
+++ hw1_2/hw1_05/main.py
@@ -0,0 +1,31 @@
+import sys
+from GUI import Ui_MainWindow
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QFileDialog
+from Q5_TrainClassifier import showCifar10, showMdlStructure, showAugmentation, showAccLoss, predict
+
+imgPath = None
+
+def loadImg():
+    global imgPath
+    imgPath = QFileDialog.getOpenFileName(directory="../")
+    return
+
+if __name__ == "__main__":
+    # GUI window
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+
+    # BtnClick
+    ui.btn_loadImg.clicked.connect(loadImg)
+    ui.btn_showImg.clicked.connect(showCifar10)
+    ui.btn_showMdlStruc.clicked.connect(showMdlStructure)
+    ui.btn_showDataAug.clicked.connect( lambda: showAugmentation(imgPath[0], ui.label) )
+    ui.btn_showAccLoss.clicked.connect(showAccLoss)
+    ui.btn_inference.clicked.connect( lambda: predict(imgPath[0], ui.lbl_conf, ui.lbl_prediction) )
+
+    # Show & Exit
+    MainWindow.show()
+    sys.exit(app.exec_())
