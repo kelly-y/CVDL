59897e6e0cbc5cd76f23cd92b8adadf34411c4c5
kelly-y <kellyyang@csie.io>
2022-10-29 18:49:10 +0800

    Complete hw1-05

--- hw1_2/hw1_05/Q5_TrainClassifier.py
+++ hw1_2/hw1_05/Q5_TrainClassifier.py
@@ -1,5 +1,9 @@
+import os
+import numpy as np
 import torch
 import torchvision
+import torch.nn as nn
+import torch.nn.functional as F
 import torchvision.transforms as transforms
 from torchsummary import summary
 import matplotlib.pyplot as plt
@@ -8,11 +12,9 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import QPixmap
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-uiWidth = 361
-uiHeight = 351
-
-def train():
-    pass
+uiWidth = 391
+uiHeight = 371
+model = None
 
 def showCifar10():
     transform = transforms.Compose( [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))] )
@@ -33,8 +35,12 @@ def showCifar10():
     plt.show()
 
 def showMdlStructure():
-    model = torchvision.models.vgg19()
-    model.to(device)
+    global model
+    if not model:
+        model = torchvision.models.vgg19(pretrained=False)
+        model.classifier[6] = nn.Linear(4096, 10)
+        model.to(device)
+        model.load_state_dict( torch.load( os.path.join(os.getcwd(), "model_Final.pth") ) )
     summary(model, (3, 32, 32))
 
 def showAugmentation(imgPath, uiShow):
@@ -68,8 +74,40 @@ def showAugmentation(imgPath, uiShow):
 
     plt.show()
 
-def showAccLoss():
-    pass
+def showAccLoss(uiShow):
+    imgPath = os.path.join(os.getcwd(), "AccLoss_Final.png")
+    pixmap = QPixmap(imgPath)
+    pixmap = pixmap.scaled(uiWidth, uiHeight, aspectRatioMode=Qt.IgnoreAspectRatio)
+    uiShow.setPixmap(pixmap)
 
 def predict(imgPath, uiShow, lblConf, lblPred):
-    pass
+    # Show
+    pixmap = QPixmap(imgPath)
+    pixmap = pixmap.scaled(uiWidth, uiHeight, aspectRatioMode=Qt.IgnoreAspectRatio)
+    uiShow.setPixmap(pixmap)
+
+    # Load model
+    global model
+    if not model:
+        model = torchvision.models.vgg19(pretrained=False)
+        model.classifier[6] = nn.Linear(4096, 10)
+        model.to(device)
+        model.load_state_dict( torch.load( os.path.join(os.getcwd(), "model_Final.pth") ) ) 
+    model.eval()
+    classes = ["airplane", "automobile", "bird", "cat", "deef", "dog", "frog", "horse", "ship", "truck"]
+
+    # Prediction and Confidence
+    img = Image.open(imgPath)   # Open image
+    img = img.resize((32, 32))
+    imgArr = np.array(img)      # Convert to numpy array
+    imgArr = np.expand_dims(imgArr, axis=0)     # Format
+    input = torch.Tensor(imgArr).permute(0, 3, 1, 2).to(device)
+    output = model(input)       # Predict
+    _, predict = torch.max(output.data, 1)
+    sftmx = F.softmax(output, dim=1)[0]
+    conf = sftmx[predict].item()
+    ans = classes[predict]      # Class Name
+
+    # Show
+    lblConf.setText("Confidence = " + str( round(conf, 2) ))
+    lblPred.setText("Prediction Label: " + ans)
